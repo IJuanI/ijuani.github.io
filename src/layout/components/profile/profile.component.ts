2d8ab02e2567f0a17468760d086015e555dd6ca2
IJuanI <juancojcl@hotmail.com>
2019-05-15 14:10:38 -0300

    Card and Format

--- src/layout/components/profile/profile.component.ts
+++ src/layout/components/profile/profile.component.ts
@@ -3,32 +3,12 @@ import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styles: [
-    `
-      .custom-txt {
-        font-size: calc(4vw - 10px);
-      }
-      @media only screen and (max-width: 768px) {
-        .custom-txt {
-          font-size: 20px;
-        }
-      }
-      .ghost-text {
-        font-size: 0.5em;
-        letter-spacing: 0.1em;
-        position: absolute;
-        margin-left: 15%;
-      }
-    `
-  ]
+  styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements AfterViewInit {
-  @ViewChild('txt0', { read: ElementRef })
-  txt0: ElementRef;
+export class ProfileComponent {
+  randomText: number;
 
-  txt0_X: 0;
-
-  ngAfterViewInit() {
-    this.txt0_X = this.txt0.nativeElement.offsetLeft;
+  constructor() {
+    this.randomText = Math.trunc(Math.random() * 4);
   }
 }
--- src/layout/layout.module.ts
+++ src/layout/layout.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { LayoutComponent } from './layout.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
-  declarations: [LayoutComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [LayoutComponent, ProfileComponent, ProjectsComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, PerfectScrollbarModule, MatTabsModule],
   providers: [],
   bootstrap: [LayoutComponent]
 })
